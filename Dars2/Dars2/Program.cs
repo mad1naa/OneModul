6acce6ec8fa2095e51b90e4bf32fa850bce30974
madinaqurbonaliyevna-hash <madinaqurbonaliyevna@gmail.com>
2025-12-05 01:08:43 +0300

    dars

--- Dars2/Dars2/Program.cs
+++ Dars2/Dars2/Program.cs
@@ -4,7 +4,316 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You are stupid");
+            */
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 0 && Natural % 2 == 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 10 && Natural < 99)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 1000 && Natural < 9999)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 10 && Natural < 99 && Natural % 2 != 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 100 && Natural < 999 && Natural % 2 == 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural > 1000 && Natural < 9999 && Natural % 2 != 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+            if (Natural % 2 == 1 && FirstNatural % 2 == 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+            if (Natural % 2 != 1 && FirstNatural % 2 != 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+            if (Natural % 2 == 1 && FirstNatural % 2 == 1 && Natural > 10 && Natural < 99 && FirstNatural > 10 && FirstNatural < 99)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            int resultOne = (Natural / 1) % 10;
+            int resultTwo = (Natural / 10) % 10;
+
+            if (Natural > 10 && Natural < 99 && resultOne + resultTwo > 10)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            int resultOne = (Natural / 1) % 10;
+            int resultTwo = (Natural / 10) % 10;
+
+            if (Natural > 10 && Natural < 99 && resultOne * resultTwo > 20)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            int resultOne = (Natural / 1) % 10;
+            int resultTwo = (Natural / 10) % 10;
+
+            if (Natural > 10 && Natural < 99 && resultOne % 2 == 1 && resultTwo % 2 == 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            int resultOne = (Natural / 1) % 10;
+            int resultTwo = (Natural / 10) % 10;
+
+            if (Natural > 10 && Natural < 99 && resultOne > 5 && resultTwo > 5)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural / 3 == 0)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+
+            if (Natural / 7 == 0)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+            if (Natural == FirstNatural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int SecondNatural = int.Parse(Console.ReadLine());
+
+            if (Natural > FirstNatural && Natural > SecondNatural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int SecondNatural = int.Parse(Console.ReadLine());
+
+            if (SecondNatural > FirstNatural && SecondNatural > Natural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int SecondNatural = int.Parse(Console.ReadLine());
+
+            if (FirstNatural < Natural && FirstNatural < SecondNatural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+
+
+            if (FirstNatural * 2 * 2 < Natural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Write("Son kiriting : ");
+            int Natural = int.Parse(Console.ReadLine());
+            Console.Write("Son kiriting : ");
+            int FirstNatural = int.Parse(Console.ReadLine());
+
+
+
+            if (FirstNatural * 5 * 5 < Natural)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+            */
+            
+
         }
     }
 }
